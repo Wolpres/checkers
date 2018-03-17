645b419b3d8fd2ba8ad6a9011fe53b8ec7baae6b
Wolpres <josefdostal@hotmail.com>
2018-03-17 10:41:13 +0100

    First Commit

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# DAMA
+
+### Author: Josef Dostál
+### Date: 3/2018
+
+### Description:
+.
+.
+.
---
+++ src/main.cpp
@@ -0,0 +1,129 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <iostream>
+//#include "Connector.hpp"
+
+using namespace std;
+using namespace sf;
+
+int size = 56;
+Vector2f offset(28,28);
+
+Sprite f[16]; //figures
+std::string position="";
+
+int board[8][8] = {
+	{ 0,-1, 0,-1, 0,-1, 0,-1},
+	{-1, 0,-1, 0,-1, 0,-1, 0},
+	{ 0, 0, 0, 0, 0, 0, 0, 0},
+	{ 0, 0, 0, 0, 0, 0, 0, 0},
+	{ 0, 0, 0, 0, 0, 0, 0, 0},
+	{ 0, 0, 0, 0, 0, 0, 0, 0},
+	{ 0, 1, 0, 1, 0, 1, 0, 1},
+	{ 1, 0, 1, 0, 1, 0, 1, 0} };
+
+int movable_possitions[8][8] = {
+	{ 0, 1, 0, 1, 0, 1, 0, 1 },
+	{ 1, 0, 1, 0, 1, 0, 1, 0 },
+	{ 0, 1, 0, 1, 0, 1, 0, 1 },
+	{ 1, 0, 1, 0, 1, 0, 1, 0 },
+	{ 0, 1, 0, 1, 0, 1, 0, 1 },
+	{ 1, 0, 1, 0, 1, 0, 1, 0 },
+	{ 0, 1, 0, 1, 0, 1, 0, 1 },
+	{ 1, 0, 1, 0, 1, 0, 1, 0 }};
+
+void loadPosition() {
+	int k = 0;
+	for (int i = 0 ; i < 8 ; i++)
+		for (int j = 0 ; j < 8 ; j++) {
+			int n = board[i][j];
+			if (!n)
+				continue;
+			int x = abs(n) - 1;
+			int y = n > 0 ? 1 : 0;
+			f[k].setTextureRect(IntRect(size * x, size * y, size, size));
+			f[k].setPosition(size * j, size * i);
+			k++;
+		}
+
+	for(int i = 0 ; i < position.length() ; i += 5)
+		move(position.substr(i, 4));
+}
+
+//TODO
+bool canMove(float x, float y) {
+	return true;
+}
+
+//TODO
+bool canJump(float x, float y) {
+
+	return false;
+}
+
+int main() {
+/** DECLARATIONS **/
+	RenderWindow window(VideoMode(504, 504), "The Chess! (press SPACE)");
+
+	Texture t1, t2;
+	t1.loadFromFile("images/pawns.png");
+	t2.loadFromFile("images/board.png");
+
+
+	for(int i = 0 ; i < 16 ; i++)
+		f[i].setTexture(t1);
+	Sprite sBoard(t2);
+
+	loadPosition();
+
+	float dx = 0, dy = 0;
+	int n = 15;
+
+/*** MAIN CYCLE ***/
+	while (window.isOpen()) {
+		Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
+
+		Event e;
+		while (window.pollEvent(e)) {
+    	    if (e.type == Event::Closed)
+				window.close();
+		}
+
+/** DRAG **/
+			if (e.type == Event::MouseButtonPressed) {
+				for(int i = 0 ; i < 16 ; i++)
+					if (f[i].getGlobalBounds().contains(pos.x, pos.y)) {
+						n = i;
+						dx = pos.x - f[i].getPosition().x;
+						dy = pos.y - f[i].getPosition().y;
+						cout << "Neco se stalo " << i << endl;
+					}
+			}
+
+/** DROP **/
+			if (e.type == Event::MouseButtonReleased) {
+				if (canMove((Mouse::getPosition().x - Vector2f(offset).x), (Mouse::getPosition().y - Vector2f(offset).y))) {
+					Vector2f tmp(Mouse::getPosition(window) - Vector2i(size/2, size/2));
+					f[n].setPosition(tmp);
+
+				}
+				if (canJump(Mouse::getPosition().x, Mouse::getPosition().y)) {
+					//TODO
+				}
+			}
+
+/** DRAW **/
+		window.clear();
+    	window.draw(sBoard);
+		for (int i = 0 ; i < 16 ; i++)
+			f[i].move(offset);
+    	for (int i = 0 ; i < 16 ; i++)
+			window.draw(f[i]);
+		window.draw(f[n]);
+		for (int i = 0 ; i < 16 ; i++)
+			f[i].move(-offset);
+ 		window.display();
+	} // while (window.isOpen())
+
+    return 0;
+}
